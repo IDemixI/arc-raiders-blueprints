b16ad62e55b4c090a420fd262046c4e5185e554f
Ryan Saunders <ryans@tassat.com>
2026-01-14 16:51:44 +0000

    Reverted changes

--- app/page.tsx
+++ app/page.tsx
@@ -59,7 +59,6 @@ export default function Home() {
             Documentation
           </a>
         </div>
-        <span style="width:100%;">This is a test</span>
       </main>
     </div>
   );
